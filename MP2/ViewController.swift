5134112fe673c9afd0fbb8afa1bbb6e34a9c3473
bijiabo <bijiabo@gmail.com>
2015-06-18 21:46:53 +0800

    Initial Commit

---
+++ MP2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MP2
+//
+//  Created by bijiabo on 15/6/18.
+//  Copyright (c) 2015年 JYLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
